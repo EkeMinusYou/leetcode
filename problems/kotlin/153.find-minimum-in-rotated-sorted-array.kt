e8266d693f4a1ea537a58ef15a57c992cf8eae3f
EkeMinusYou <euonymuslke+develop@gmail.com>
2022-03-28 20:51:23 +0900

    153: fix performane

--- problems/kotlin/153.find-minimum-in-rotated-sorted-array.kt
+++ problems/kotlin/153.find-minimum-in-rotated-sorted-array.kt
@@ -7,7 +7,31 @@
 // @lc code=start
 class Solution {
     fun findMin(nums: IntArray): Int {
-        return nums.sorted().first()
+        if (nums.size == 1) return nums.first()
+        if (nums.first() < nums.last()) return nums.first()
+
+        var left = 0
+        var right = nums.size - 1
+
+        while (left <= right) {
+            var mid = left + (right - left) / 2
+
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1]
+            }
+
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid]
+            }
+
+            if (nums[0] < nums[mid]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+        return -1
     }
 }
 // @lc code=end
